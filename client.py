51f0b1977c78d5d18269e917a1668d0c92767301
MaxShmorgun <makssmorgun1@gmail.com>
2025-08-31 15:14:42 +0300

    Add music

--- client.py
+++ client.py
@@ -4,31 +4,35 @@ import json
 from threading import Thread
 from menu import ConnectWindow
 
+# --- Ініціалізація звуку ---
+mixer.init()
+
+# --- Вікно підключення ---
 win = ConnectWindow()
 win.mainloop()
 
-host = win.host 
-port = win.port 
+host = win.host
+port = win.port
 
-# ---ПУГАМЕ НАЛАШТУВАННЯ ---
+# --- PYGAME НАЛАШТУВАННЯ ---
 WIDTH, HEIGHT = 800, 600
 init()
 screen = display.set_mode((WIDTH, HEIGHT))
 clock = time.Clock()
 display.set_caption("Пінг-Понг")
-# ---СЕРВЕР ---
+
+# --- СЕРВЕР ---
 def connect_to_server():
     while True:
         try:
             client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            client.connect((host, port)) # ---- Підключення до сервера
+            client.connect((host, port))
             buffer = ""
             game_state = {}
             my_id = int(client.recv(24).decode())
             return my_id, game_state, buffer, client
         except:
             pass
-
 
 def receive():
     global buffer, game_state, game_over
@@ -47,16 +51,30 @@ def receive():
 # --- ШРИФТИ ---
 font_win = font.Font(None, 72)
 font_main = font.Font(None, 36)
-# --- ЗОБРАЖЕННЯ ---
 
+# --- ЗОБРАЖЕННЯ ---
+tennis_paddle_green = image.load("image/tennis_paddle_green.png")
+tennis_paddle_green = transform.scale(tennis_paddle_green, (20, 100))
+tennis_paddle = image.load("image/tennis_paddle.png")
+tennis_paddle = transform.scale(tennis_paddle, (20, 100))
 tennis_ball = image.load("image/tennis_ball.png")
 tennis_ball = transform.scale(tennis_ball, (20, 20))
+image_bg = image.load("image/table_tennis.png")
+image_bg = transform.scale(image_bg, (WIDTH, HEIGHT))
 
-image = image.load("image/table_tennis.png")
-image = transform.scale(image, (WIDTH, HEIGHT))
+# --- ЗВУКИ ---
+mixer.music.load("music/tennis_misic.mp3")
+bouncing_the_ball = mixer.Sound("music/tennis_ball_mus.wav")
+bouncing_the_ball.set_volume(1)
+bouncing_a_ball_off_a_wall = mixer.Sound("music/wall.wav")
+bouncing_a_ball_off_a_wall.set_volume(1)
+winner_sound = mixer.Sound("music/win.wav")
+winner_sound.set_volume(1)
+loser_sound = mixer.Sound("music/lose.wav")
+loser_sound.set_volume(1)
 
 
-# -- ЗВУКИ ---
+music_playing = False
 
 # --- ГРА ---
 game_over = False
@@ -64,27 +82,40 @@ winner = None
 you_winner = None
 my_id, game_state, buffer, client = connect_to_server()
 Thread(target=receive, daemon=True).start()
+
 while True:
     for e in event.get():
         if e.type == QUIT:
             exit()
-    
+
+    if not music_playing:
+        mixer.music.play(-1)
+        music_playing = True
 
     if "countdown" in game_state and game_state["countdown"] > 0:
+        if music_playing:
+            mixer.music.stop()
+            music_playing = False
+
         screen.fill((0, 0, 0))
         countdown_text = font.Font(None, 72).render(str(game_state["countdown"]), True, (255, 255, 255))
         screen.blit(countdown_text, (WIDTH // 2 - 20, HEIGHT // 2 - 30))
         display.update()
-        continue  # Не малюємо гру до завершення відліку
+        continue
 
     if "winner" in game_state and game_state["winner"] is not None:
-        screen.fill((20, 20, 20))
+        if not music_playing:
+            mixer.music.play(-1)
+            music_playing = True
 
-        if you_winner is None:  # Встановлюємо тільки один раз
+        screen.fill((20, 20, 20))
+        if you_winner is None:
             if game_state["winner"] == my_id:
                 you_winner = True
+                winner_sound.play()
             else:
                 you_winner = False
+                loser_sound.play()
 
         if you_winner:
             text = "Ти переміг!"
@@ -100,28 +131,29 @@ while True:
         screen.blit(text, text_rect)
 
         display.update()
-        continue  # Блокує гру після перемоги
+        continue
 
     if game_state:
-        screen.blit(image,(0, 0))
-        draw.rect(screen, (0, 255, 0), (20, game_state['paddles']['0'], 20, 100))
-        draw.rect(screen, (255, 0, 255), (WIDTH - 40, game_state['paddles']['1'], 20, 100))
+        if music_playing:
+            mixer.music.stop()
+            music_playing = False
+
+        screen.blit(image_bg, (0, 0))
+        screen.blit(tennis_paddle, (20, game_state['paddles']['0']))
+        screen.blit(tennis_paddle_green, (WIDTH - 40, game_state['paddles']['1']))
         screen.blit(tennis_ball, (game_state['ball']['x'] - 10, game_state['ball']['y'] - 10))
         score_text = font_main.render(f"{game_state['scores'][0]} : {game_state['scores'][1]}", True, (255, 255, 255))
-        screen.blit(score_text, (WIDTH // 2 -25, 20))
+        screen.blit(score_text, (WIDTH // 2 - 25, 20))
 
         if game_state['sound_event']:
             if game_state['sound_event'] == 'wall_hit':
-                # звук відбиття м'ячика від стін
-                pass
+                bouncing_a_ball_off_a_wall.play()
             if game_state['sound_event'] == 'platform_hit':
-                # звук відбиття м'ячика від платформи
-                pass
-
+                bouncing_the_ball.play()
     else:
-        wating_text = font_main.render(f"Очікування гравців...", True, (255, 255, 255))
-        screen.blit(wating_text, (WIDTH // 2 - 25, 20))
-    
+        # Відображення тексту "Очікування гравців..." в режимі очікування
+        waiting_text = font_main.render(f"Очікування гравців...", True, (255, 255, 255))
+        screen.blit(waiting_text, (WIDTH // 2 - 25, 20))
 
     display.update()
     clock.tick(60)
--- menu.py
+++ menu.py
@@ -4,30 +4,25 @@ import sys
 class ConnectWindow(CTk):
     def __init__(self):
         super().__init__()
-
+        
         self.name = None
         self.host = None
         self.port = None
 
-        
         self.title("Ping-Pong")
         self.geometry("300x400")
         self.resizable(False, False)
         self.configure(fg_color="#1a1a1a")
-        
-        
+
         self.protocol("WM_DELETE_WINDOW", self.on_closing)
 
-        
         self.grid_rowconfigure((0, 1, 2, 3, 4), weight=1)
         self.grid_columnconfigure(0, weight=1)
 
-        
         CTkLabel(self, text="Підключення до Ping-Pong", 
                  font=("Comic Sans MS", 22, 'bold'),
                  text_color="#f0f0f0").grid(row=0, column=0, pady=(20, 10))
 
-        
         self.host_entry = CTkEntry(self, placeholder_text="Введіть хост", 
                                    height=45, fg_color="#2b2b2b", text_color="#f0f0f0",
                                    border_color="#4f4f4f", border_width=2, corner_radius=10)
@@ -38,13 +33,11 @@ class ConnectWindow(CTk):
                                    border_color="#4f4f4f", border_width=2, corner_radius=10)
         self.port_entry.grid(row=2, column=0, padx=30, pady=10, sticky="ew")
 
-        
         CTkButton(self, text="Приєднатися", 
                   command=self.open_game, 
                   height=50, font=("Arial", 16, 'bold'),
                   fg_color="#0066cc", hover_color="#0088e8", corner_radius=10).grid(row=3, column=0, pady=20, padx=30, sticky="ew")
-        
-        
+
         self.status_label = CTkLabel(self, text="", text_color="#ff4d4d", font=("Arial", 12))
         self.status_label.grid(row=4, column=0, pady=5)
 
@@ -55,12 +48,11 @@ class ConnectWindow(CTk):
             self.destroy()
         except ValueError:
             self.status_label.configure(text="Порт має бути числом!")
-            
-    
+
     def on_closing(self):
         print("Вікно меню закрито. Завершення програми.")
         self.destroy()
-        sys.exit()    
+        sys.exit() 
 
 if __name__ == "__main__":
     app = ConnectWindow()
